c1386b1f84af457dfeca9d504f8e5a1ee94db410
Muhammad Ikhsanudin <muhikhsanudin1@gmail.com>
2022-06-19 14:05:42 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MountdakiApp());
+}
+
+class MountdakiApp extends StatelessWidget {
+  const MountdakiApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
